1666f4a058025c50b401e6e6720162bde1768f98
Mahdyar <mahdyaralipor0@gmail.com>
2023-11-23 12:45:22 +0330

    update

--- core/controller/contact-usController.php
+++ core/controller/contact-usController.php
@@ -2,14 +2,16 @@
 if(isset($_POST['btn'])){
     try{
         $name    = $_POST['name'];   
-        $subject = $_POST['theme'];
+        $subject = $_POST['subject'];
         $email   = $_POST['email'];
         $message = $_POST['message'];
 
-        $query = 'INSERT INTO `contact-us` (`id`, `message`, `email`, `theme`) VALUES (NULL, ?, ?,?)';
+        $query = 'INSERT INTO `contact-us` ( `name`,`subject`,`email`, `message`) VALUES (?, ?,?,?)';
         $result = connection()->prepare($query);
-        $result->bindValue(1,$email);
-        $result->bindValue(2,$message);
+        $result->bindValue(1,$name);
+        $result->bindValue(2,$subject);
+        $result->bindValue(3,$email);
+        $result->bindValue(4,$message);
         if($result->execute()){}
     }
     catch(Exception $e){
