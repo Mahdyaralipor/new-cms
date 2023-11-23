18439fa910ce0af2e76bb5e3647bdf96c9de4602
Mahdyar <mahdyaralipor0@gmail.com>
2023-11-23 12:27:31 +0330

    Fix code format

--- core/controller/contact-usController.php
+++ core/controller/contact-usController.php
@@ -1,19 +1,18 @@
 <?php
 if(isset($_POST['btn'])){
+    try{
+        $name    = $_POST['name'];   
+        $subject = $_POST['theme'];
+        $email   = $_POST['email'];
+        $message = $_POST['message'];
 
-try{
-    $name    = $_POST['name'];   
-    $subject = $_POST['theme'];
-    $email   = $_POST['email'];
-    $message = $_POST['message'];
-
-    $query = 'INSERT INTO `contact-us` (`id`, `message`, `email`, `theme`) VALUES (NULL, ?, ?,?)';
-    $result = connection()->prepare($query);
-    $result->bindValue(1,$email);
-    $result->bindValue(2,$message);
-    if($result->execute()){}
-}
-catch(Exception $e){
-    die($e->getMessage());
-}
+        $query = 'INSERT INTO `contact-us` (`id`, `message`, `email`, `theme`) VALUES (NULL, ?, ?,?)';
+        $result = connection()->prepare($query);
+        $result->bindValue(1,$email);
+        $result->bindValue(2,$message);
+        if($result->execute()){}
     }
+    catch(Exception $e){
+        die($e->getMessage());
+    }
+}
