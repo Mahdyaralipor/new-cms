de1a2255ed1fa7a139a63b1585e3a566b47f2af5
Mahdyar <mahdyaralipor0@gmail.com>
2023-11-23 13:20:28 +0330

    Update project

--- core/src/router.php
+++ core/src/router.php
@@ -3,18 +3,23 @@ require_once 'functions.php';
 function route_request() {
     $uri = $_SERVER['REQUEST_URI'];
     $uri = explode('?', $uri)[0];
-
-    match ($uri) {
-        '/' => render_template('index'),
-        '/about-us' => render_template('about-us'),
-        '/contact-us' => [
-            render_controller('contact-us'),
-            render_template('contact-us')
-        ],
-        default => {
+    switch ($uri) {
+        case '/':
+            render_template('index');
+            break;
+        case '/about-us':
+            render_template('about-us');
+            break;
+        case '/about-us':
+            render_template('login');
+            break;
+        case '/contact-us':
+            render_controller('contact-us');
+            render_template('contact-us');
+            break;
+        default:
             header('HTTP/1.0 404 Not Found');
             echo '404 Not Found';
-        }
-    }; break;
+            break;
     }
 }
